320b6e78059d069d0157b34b4b629fd4be641867
EternityShen <3258551611@qq.com>
2026-04-19 16:32:07 +0800

    fix:修正一些小错误

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use eternal_memory::logger::loghandle::LogHandle;
+
 fn main() {
+    let mut log = LogHandle::new("./debug/log.log");
+    log.info("INFO Message".to_string());
+
     println!("Hello, world!");
 }
